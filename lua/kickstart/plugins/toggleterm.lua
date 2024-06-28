1f373fd6e17c4a1ad91dc5b19369def345820f06
chetelev <chetelev@gmail.com>
2024-06-28 20:01:01 +0200

    add size to toggleterm binding
    upgrade toggleterm default window size

--- lua/kickstart/plugins/toggleterm.lua
+++ lua/kickstart/plugins/toggleterm.lua
@@ -3,6 +3,7 @@ return {
   version = '*',
   config = function()
     require('toggleterm').setup {
+      insert_mapping = true,
       vim.api.nvim_set_keymap('n', '<C-t>', '<Cmd>exe v:count1 . "ToggleTerm"<CR>', { noremap = true, silent = true }),
       vim.api.nvim_set_keymap('t', '<C-t>', '<C-\\><C-n><Cmd>exe v:count1 . "ToggleTerm"<CR>', { noremap = true, silent = true }),
       vim.api.nvim_set_keymap('t', '<Esc>', '<C-\\><C-n>', { noremap = true, silent = true }),
@@ -10,6 +11,6 @@ return {
   end,
   opts = {},
   keys = {
-    { '<leader>tt', ':ToggleTerm<CR>', { desc = 'Toggle Terminal' } },
+    { '<leader>tt', ':ToggleTerm size=50<CR>', { desc = 'Toggle Terminal' } },
   },
 }
